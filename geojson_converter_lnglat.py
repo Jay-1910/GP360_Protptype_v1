f59537e7a7a22761f7c1a771bddcf8af2eacfebb
minhaj231 <minhaj.ahmed@student.adelaide.edu.au>
2024-12-17 16:46:39 +1030

    added csv and geojson files for other commodity data

--- geojson_converter_lnglat.py
+++ geojson_converter_lnglat.py
@@ -31,4 +31,4 @@ def csv_to_geojson(input_csv, output_geojson, lat_col='LATITUDE', lon_col='LONGI
     print(f"GeoJSON file has been saved to {output_geojson}")
 
 # Example usage
-csv_to_geojson('dataset/commodity_relevant_data/mineral_resource_potential.csv', 'dataset/commodity_relevant_data/mineral_resource_potential.geojson')
+csv_to_geojson('dataset/commodity/graphite_mines_and_mineral_deposits.csv', 'dataset/commodity/graphite_mines_and_mineral_deposits.geojson')
